d23bf9e37ea63f8ee4d7a6bccef4434c90ecc037
Gustavo <gustavo7dias@gmail.com>
2023-02-20 17:17:42 -0300

    Create section about coffee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      beige: "#bfa98b",
+    },
     container: {
       center: true,
       padding: {
