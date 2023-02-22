76d75405c0a701fca1abebc83b348146367b9802
Gustavo <gustavo7dias@gmail.com>
2023-02-22 17:26:53 -0300

    Create popular section, update styles and update screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,28 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     colors: {
       ...colors,
-      beige: "#bfa98b",
+      beige: {
+        50: "#e5dcd0",
+        100: "#d8cbb9",
+        200: "#ccbaa2",
+        300: "#bfa98b",
+        400: "#a69278",
+        500: "#8d7c66",
+      },
     },
     container: {
       center: true,
       padding: {
         DEFAULT: "1.5rem",
-        sm: "0rem",
+        sm: "0",
       },
     },
   },
