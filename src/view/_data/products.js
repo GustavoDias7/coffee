a30cb39348b5578e80e384e34e981ad0dbfd7c0b
Gustavo <gustavo7dias@gmail.com>
2023-02-23 15:51:48 -0300

    Update popular dishes

---
+++ src/view/_data/products.js
@@ -0,0 +1,52 @@
+module.exports = {
+  dishes: [
+    {
+      image: "tiramisu-cake.jpg",
+      title: "Tiramisu Cake",
+      description:
+        "Sample text. Click to select the text box. Click again or double click to start editing the text.",
+      price: "10,00",
+      link: "/",
+    },
+    {
+      image: "chia-pudding.jpg",
+      title: "Chia Pudding",
+      description:
+        "Sample text. Click to select the text box. Click again or double click to start editing the text.",
+      price: "8,00",
+      link: "/",
+    },
+    {
+      image: "cheesecake.jpg",
+      title: "Cheesecake",
+      description:
+        "Sample text. Click to select the text box. Click again or double click to start editing the text.",
+      price: "7,00",
+      link: "/",
+    },
+    {
+      image: "chocolate-cake.jpg",
+      title: "Chocolate Cake",
+      description:
+        "Sample text. Click to select the text box. Click again or double click to start editing the text.",
+      price: "11,00",
+      link: "/",
+    },
+    {
+      image: "cheesecake-2.jpg",
+      title: "Cheesecake",
+      description:
+        "Sample text. Click to select the text box. Click again or double click to start editing the text.",
+      price: "12,00",
+      link: "/",
+    },
+    {
+      image: "orange-cake.jpg",
+      title: "Orange cake",
+      description:
+        "Sample text. Click to select the text box. Click again or double click to start editing the text.",
+      price: "10,00",
+      link: "/",
+    },
+  ],
+};
