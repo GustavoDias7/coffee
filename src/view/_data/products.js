007022faa4c8542419d1e3ac1a50113bdbfa2d3e
Gustavo <gustavo7dias@gmail.com>
2024-03-08 18:24:22 -0300

    Create coffee section

--- src/view/_data/products.js
+++ src/view/_data/products.js
@@ -3,6 +3,53 @@ const { getWhatsAppLink } = require("../../js/utils/whatsapp-link");
 const link_message = "Olá, gostaria de fazer o pedido do prato";
 
 module.exports = {
+  coffees: [
+    {
+      image: "americano.jpg",
+      title: "Americano",
+      description:
+        "O café Americano é um café feito diluindo o expresso em água quente. O resultado é semelhante em força ao café em gotas, mas com um perfil de sabor ligeiramente diferente.",
+      price: "6,00",
+      link() {
+        return getWhatsAppLink(`${link_message} ${this.title}`);
+      },
+    },
+    {
+      image: "black-coffee.jpg",
+      title: "Café preto",
+      description:
+        "O café preto é simplesmente café sem adição de nada – sem creme, sem leite, sem adoçante. Isso permite que você aproveite os benefícios do café para a saúde sem aditivos.",
+      price: "4,00",
+    },
+    {
+      image: "cappuccino.jpg",
+      title: "Cappuccino",
+      description:
+        "Um cappuccino é a mistura específica entre uma dose de café expresso e leite com espuma. A proporção é de 1:5 de café expresso para leite, com muito sabor de expresso no leite.",
+      price: "12,00",
+    },
+    {
+      image: "expresso.jpg",
+      title: "Espresso",
+      description:
+        "Uma forma de café concentrada e cheia de sabor. É feito forçando água quente pressurizada através de grãos de café moídos muito finos em uma máquina de café expresso.",
+      price: "9,00",
+    },
+    {
+      image: "frappuccino.jpg",
+      title: "Frappuccino",
+      description:
+        "O Frappuccino é uma mistura de café gelado. Inspirada no frappe e no cappuccino, a bebida é feita com café gelado e delicioso.",
+      price: "14,00",
+    },
+    {
+      image: "macchiato.jpg",
+      title: "Macchiato",
+      description:
+        "Em termos mais básicos, é um expresso com um pouco de leite. A quantidade de leite é suficiente para clarear a cor do café e perfurar a superfície lisa e preta.",
+      price: "15,00",
+    },
+  ],
   dishes: [
     {
       image: "tiramisu-cake.jpg",
